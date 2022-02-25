8bf3dcf6a748fba45a71e18913485faefc3c0bb1
Lina Fang <lina-fang@users.noreply.github.com>
2022-02-24 16:36:24 -0800

    added ImagePicker and ProfilePictureView

--- CardinalKit/CardinalKit-Example/CardinalKit/Components/Profile/ImagePicker.swift
+++ CardinalKit/CardinalKit-Example/CardinalKit/Components/Profile/ImagePicker.swift
@@ -42,7 +42,7 @@ struct ImagePicker: UIViewControllerRepresentable {
 
             if provider.canLoadObject(ofClass: UIImage.self) {
                 provider.loadObject(ofClass: UIImage.self) { image, _ in
-                    self.parent.image = image as! UIImage
+                    self.parent.image = image as? UIImage
                 }
             }
         }
