e46c6f4ca265fb1f2dec49b7ed9e263f3339c8e3
Blynn Shideler <b.shideler@yahoo.com>
2022-03-02 23:34:15 -0800

    adding options for live therapy chart view

--- CardinalKit/CardinalKit-Example/CardinalKit/Components/Home/Live Therapy/LiveTherapyChartView.swift
+++ CardinalKit/CardinalKit-Example/CardinalKit/Components/Home/Live Therapy/LiveTherapyChartView.swift
@@ -33,17 +33,49 @@ struct LiveTherapyChartView: UIViewRepresentable {
         print("in add data")
         // convert the Doubles to BarChartDataEntry
 //        maxData.suffix(maxData from maxData.count - 6)
-        for i in 1...5 {
+        
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        // Blynn - To modify Live Therapy Chart Appearance:
+        //Option A: live data enters from right, pushing older data to left
+        let dataViewLength = 15
+        
+        for i in 1...dataViewLength {
             if i <= maxData.count {
-                var xVal = 6 - Double(i)
-                var yVal = maxData[maxData.count - i]*9.8
+                let xVal = Double(dataViewLength - i + 1)
+                let yVal = maxData[maxData.count - i]*9.8
                 maxAccelArray.append(BarChartDataEntry(x: xVal, y: yVal))
                 print("the y is \(yVal)")
+            } else {
+                let xVal = Double(dataViewLength - i + 1)
+                let yVal = 0.0
+                maxAccelArray.append(BarChartDataEntry(x: xVal, y: yVal))
             }
         }
         
+        //Option B: live data grows from left & resets every at the end of the x axis, where limits go from 0 to <dataViewLengh>
+        
+//        for i in 1...dataViewLength {
+//            if (dataViewLength + maxData.count - i) % dataViewLength == 0 {
+//                for j in 1...dataViewLength {
+//                    //let xVal = Double(j)
+//                    if j <= i {
+//                        let xVal = Double(j)
+//                        let yVal = maxData[maxData.count - i + j - 1]*9.8
+//                        maxAccelArray.append(BarChartDataEntry(x: xVal, y: yVal))
+//                    } else {
+//                        let xVal = Double(j)
+//                        let yVal = 0.0
+//                        maxAccelArray.append(BarChartDataEntry(x: xVal, y: yVal))
+//                    }
+//                }
+//            }
+//        }
+        
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        
         let dataSet = BarChartDataSet(entries : maxAccelArray)
-        dataSet.colors = [NSUIColor.green]
+        dataSet.colors = [NSUIColor(red: 255.0/255.0, green: 153.0/255.0, blue: 205.0/255.0, alpha: 1.0)]
         dataSet.label = "My Data"
         data.addDataSet(dataSet)
         return data
