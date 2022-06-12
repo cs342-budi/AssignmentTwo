b3d5571af15eb54e69794422504f9bfdf6ee32c4
Mihir Joshi <mihirjoe@gmail.com>
2022-06-12 10:44:31 -0400

    redesigned watch therapy view

--- CardinalKit/CardinalKit-Example/BUDI WatchKit WatchKit Extension/MetricsView.swift
+++ CardinalKit/CardinalKit-Example/BUDI WatchKit WatchKit Extension/MetricsView.swift
@@ -13,24 +13,64 @@ struct MetricsView: View {
     @EnvironmentObject var workoutManager: WorkoutManager
     @EnvironmentObject var cmMotionManager: CoreMotionManager
 //    @ObservedObject var manager = CoreMotionManager()
-    
+        
     var body: some View {
+       
         TimelineView(MetricsTimelineSchedule(from: workoutManager.builder?.startDate ?? Date())) { context in
-            VStack(alignment: .leading) {
+            VStack(alignment: .center) {
+                Spacer()
                 ElapsedTimeView(elapsedTime: workoutManager.builder?.elapsedTime ?? 0, showSubseconds: context.cadence == .live)
-                    .foregroundStyle(.yellow)
-                Text(Measurement(value: workoutManager.activeEnergy, unit: UnitEnergy.kilocalories)
-                        .formatted(.measurement(width: .abbreviated, usage: .workout, numberFormatStyle: .number.precision(.fractionLength(0))))).foregroundColor(.red)
-                Text(workoutManager.heartRate.formatted(.number.precision(.fractionLength(0))) + " bpm").foregroundColor(.mint)
-                Text(cmMotionManager.accelaration.formatted(.number.precision(.fractionLength(0)))+" cm/s").foregroundColor(.purple)+Text("2").font(.system(.footnote)).baselineOffset(10).foregroundColor(.purple)
+                    .foregroundStyle(.black).font(.title3)//yellow
+                Spacer()
+                VStack(alignment: .center) {
+                    HStack(alignment: .center) {
+                        Spacer()
+                        VStack {
+                            Text("5").font(.title).foregroundColor(.black)
+                            Text("Points").font(.footnote).foregroundColor(.black)
+                        }
+                        Spacer()
+                        VStack {
+                            Text(cmMotionManager.accelaration.formatted(.number.precision(.fractionLength(0)))).foregroundColor(.black)
+                            Text("cm/s").foregroundColor(.black).font(.footnote)+Text("2").font(.system(size: 8)).baselineOffset(5).foregroundColor(.black)//purple
+                        }
+                        Spacer()
+                    }
+                    Spacer()
+                    HStack {
+                        Spacer()
+                        VStack(alignment: .leading) {
+                            Spacer()
+                            HStack {
+                                Image(systemName: "flame.fill").foregroundColor(.black).imageScale(.small)
+                                Text(Measurement(value: workoutManager.activeEnergy, unit: UnitEnergy.kilocalories).formatted(.measurement(width: .abbreviated, usage: .workout, numberFormatStyle: .number.precision(.fractionLength(0))))).foregroundColor(.black).font(.caption)
+                            }
+                            Spacer()
+                            HStack {
+                                Image(systemName: "heart.fill").foregroundColor(.black).imageScale(.small)
+                                Text(workoutManager.heartRate.formatted(.number.precision(.fractionLength(0))) + " bpm").foregroundColor(.black).font(.caption)
+                            }
+                            //red
+                            Spacer()
+                            //mint
+                            Spacer()
+                        }
+                        Spacer()
+                    }
+                    
+                    Spacer()
+
+                }
+                
 //                Text(Measurement(value: workoutManager.distance, unit: UnitLength.meters).formatted(.measurement(width: .abbreviated, usage: .road)))
             }
             .font(.system(.title, design: .rounded).monospacedDigit().lowercaseSmallCaps())
             .frame(maxWidth: .infinity, alignment: .leading)
             .ignoresSafeArea(edges: .bottom)
             .scenePadding()
-            .background(Color.gray)
+            .background(Color.green)
         }
+        
     }
 }
 
